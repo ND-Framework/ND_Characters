142dc082741e1b4816d3d99231d53d5c77101370
ParuThePro <35810485+paruthepro@users.noreply.github.com>
2025-02-01 20:20:32 +0000

    Salary by rank additions

--- data/salaries.lua
+++ data/salaries.lua
@@ -1,27 +1,125 @@
 return {
     ["default"] = { -- default salary characters receive with none of these jobs.
         enabled = true,
-        interval = 24,
-        amount = 300
+        interval = 15,
+        amount = 300,
+        rank = {
+            [1] = 50
+        }
     },
-    ["sahp"] = {
+    ["sasp"] = {
         enabled = true,
-        interval = 24,
-        amount = 700
+        interval = 15,
+        amount = 500,
+        rank = {
+            [1] = 325,
+            [2] = 450,
+        }
     },
-    ["lspd"] = {
+    ["police"] = {
         enabled = true,
-        interval = 24,
-        amount = 600
+        interval = 15,
+        amount = 500,
+        rank = {
+            [1] = 325,
+            [2] = 375,
+            [3] = 400,
+            [4] = 460,
+            [5] = 525,
+            [6] = 545,
+            [7] = 600,
+            [8] = 625,
+            [9] = 700,
+            [10] = 800,
+        }
     },
     ["bcso"] = {
         enabled = true,
-        interval = 24,
-        amount = 500
+        interval = 15,
+        amount = 500,
+        rank = {
+            [1] = 325,
+            [2] = 375,
+            [3] = 400,
+            [4] = 460,
+            [5] = 525,
+            [6] = 545,
+            [7] = 600,
+            [8] = 625,
+            [9] = 700,
+            [10] = 800,
+        }
+    },
+    ["doc"] = {
+        enabled = true,
+        interval = 15,
+        amount = 500,
+        rank = {
+            [1] = 325,
+            [2] = 375,
+            [3] = 400,
+            [4] = 460,
+            [5] = 525,
+            [6] = 545,
+            [7] = 600,
+            [8] = 625,
+            [9] = 700,
+            [10] = 800,
+            [11] = 850,
+            [12] = 900,
+        }
+    },
+    ["srt"] = {
+        enabled = true,
+        interval = 15,
+        amount = 500,
+        rank = {
+            [1] = 525,
+            [2] = 545,
+            [3] = 600,
+            [4] = 625,
+            [5] = 700,
+            [6] = 800,
+        }
     },
     ["lsfd"] = {
         enabled = true,
-        interval = 24,
-        amount = 800
+        interval = 15,
+        amount = 800,
+        rank = {
+            [1] = 225,
+            [2] = 345,
+            [3] = 400,
+            [4] = 525,
+            [5] = 600,
+            [6] = 1000,
+        }
+    },
+    ["sams"] = {
+        enabled = true,
+        interval = 15,
+        amount = 800,
+        rank = {
+            [1] = 425,
+            [2] = 545,
+            [3] = 600,
+            [4] = 725,
+            [5] = 875,
+            [6] = 900,
+        }
+    },
+    ["doj"] = {
+        enabled = true,
+        interval = 15,
+        amount = 1200,
+        rank = {
+            [1] = 425,
+            [2] = 545,
+            [3] = 600,
+            [4] = 725,
+            [5] = 875,
+            [6] = 900,
+        }
     }
 }
+
